c80ff6256189e9c03fdc142beeac9047ef5b9370
muhammaduzairrazaq <muhammaduzairrazaq@gmail.com>
2023-10-09 12:36:14 +0500

    project completed

--- homepage/views.py
+++ homepage/views.py
@@ -1,9 +1,17 @@
 from django.template import loader
 from django.http import HttpResponse
 from django.shortcuts import render
+import datetime as dt
 
 # Create your views here.
 
 def display(request):
+    a = 'NO!'
+    time = dt.datetime.now()
+    if time.month == 8 and time.day == 14:
+        a = 'YES!'
     template = loader.get_template('independence.html')
-    return HttpResponse(template.render())
+    context = {
+        'display': a,
+    }
+    return HttpResponse(template.render(context,request))
